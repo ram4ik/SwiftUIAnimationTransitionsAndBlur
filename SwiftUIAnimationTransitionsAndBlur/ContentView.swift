05e5d2addc43bb3e985f58efa223225f05d7c74a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-21 17:33:40 +0300

    init

--- SwiftUIAnimationTransitionsAndBlur/ContentView.swift
+++ SwiftUIAnimationTransitionsAndBlur/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
-        Text("Hello World")
+        
+        VStack {
+            
+            Text("Learning SwiftUI")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.orange)
+                .padding(4)
+                .animation(Animation.easeInOut(duration: 0.3))
+            
+            Image("ui")
+                .frame(width: show ? 414 : 300, height: show ? 600 : 300)
+                .clipped()
+                .cornerRadius(30)
+                .blur(radius: show ? 0 : 30)
+                .animation(.spring())
+            
+            Text("A course focused on UI")
+                .font(.subheadline)
+                .fontWeight(.regular)
+                .foregroundColor(.purple)
+                .padding(4)
+                .animation(Animation.easeIn(duration: 0.4))
+            
+            Button(action: {
+                withAnimation{
+                    self.show.toggle()
+                }
+            }) {
+                Text("Animate")
+            }
+        }
     }
 }
 
